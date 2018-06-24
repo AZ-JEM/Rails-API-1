a9df6767f37c72f6c3d51b956b5b51b20baa7695
Jeff McCarley <txazjem@gmail.com>
2018-06-24 12:54:15 -0700

    Public CORS API configuration

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,17 @@ module Tdapi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # ---------------------------------------------------------------------- /
+    # JEM : 180624
+    # completely PUBLIC API
+    # ---------------------------------------------------------------------- /
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
