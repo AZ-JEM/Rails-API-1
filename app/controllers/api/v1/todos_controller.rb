86d94d61ea7141f39336eb60474a97e3ef446a6f
Jeff McCarley <txazjem@gmail.com>
2018-06-25 02:09:50 -0700

    CRUD for Todos

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,10 +1,64 @@
 module Api
   module V1
     class TodosController < ApplicationController
+      # ---------------------------------------------------------------------- /
+      # LIST :
+      # ---------------------------------------------------------------------- /
       def index
         todos = Todo.all
-        render json: {status: 'SUCCESS', message: 'Todos index', data: todos}, status: :ok
+        render json: {status: 'SUCCESS', message: 'index todos', data: todos}, status: :ok
       end
+
+      # ---------------------------------------------------------------------- /
+      # CREATE
+      # ---------------------------------------------------------------------- /
+      def create
+        todo = Todo.new(todo_params)
+        if todo.save
+          render json: {status: 'SUCCESS', message: 'todo created', data: todo}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'create todo failed', data: todo.errors }, status: :unprocessable_entity
+        end
+      end
+
+      # ---------------------------------------------------------------------- /
+      # READ
+      # ---------------------------------------------------------------------- /
+      def show
+        todo = Todo.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'show todo', data: todo}, status: :ok
+      end
+
+      # ---------------------------------------------------------------------- /
+      # UPDATE
+      # ---------------------------------------------------------------------- /
+      def update
+        todo = Todo.find(params[:id])
+        if todo.update_attributes(todo_params)
+          render json: {status: 'SUCCESS', message: 'todo updated', data: todo}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'update todo failed', data: todo.errors }, status: :unprocessable_entity
+        end
+      end
+
+      # ---------------------------------------------------------------------- /
+      # DELETE
+      # ---------------------------------------------------------------------- /
+      def destroy
+        todo = Todo.find(params[:id])
+        todo.destroy
+        render json: {status: 'SUCCESS', message: 'todo deleted', data: todo}, status: :ok
+      end
+
+      private
+
+      # ---------------------------------------------------------------------- /
+      # "white-list" parameters
+      # ---------------------------------------------------------------------- /
+      def todo_params
+        params.permit(:title, :desc, :done)
+      end
+
     end
   end
 end
