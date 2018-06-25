590625221445ef13f724ef27e9c04ae48cefd2a4
Jeff McCarley <txazjem@gmail.com>
2018-06-25 02:39:48 -0700

    DRY CRUD

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,23 +1,26 @@
 module Api
   module V1
     class TodosController < ApplicationController
+
+      before_action :select_todo, only: [:show, :update, :destroy]
+
       # ---------------------------------------------------------------------- /
       # LIST :
       # ---------------------------------------------------------------------- /
       def index
         todos = Todo.all
-        render json: {status: 'SUCCESS', message: 'index todos', data: todos}, status: :ok
+        render json: {status: 'SUCCESS', message: 'todos loaded', data: todos}, status: :ok
       end
 
       # ---------------------------------------------------------------------- /
       # CREATE
       # ---------------------------------------------------------------------- /
       def create
-        todo = Todo.new(todo_params)
-        if todo.save
-          render json: {status: 'SUCCESS', message: 'todo created', data: todo}, status: :ok
+        @todo = Todo.new(todo_params)
+        if @todo.save
+          render json: {status: 'SUCCESS', message: 'todo created', data: @todo}, status: :ok
         else
-          render json: {status: 'ERROR', message: 'create todo failed', data: todo.errors }, status: :unprocessable_entity
+          render json: {status: 'ERROR', message: 'create todo failed', data: @todo.errors }, status: :unprocessable_entity
         end
       end
 
@@ -25,19 +28,17 @@ module Api
       # READ
       # ---------------------------------------------------------------------- /
       def show
-        todo = Todo.find(params[:id])
-        render json: {status: 'SUCCESS', message: 'show todo', data: todo}, status: :ok
+        render json: {status: 'SUCCESS', message: 'show todo', data: @todo}, status: :ok
       end
 
       # ---------------------------------------------------------------------- /
       # UPDATE
       # ---------------------------------------------------------------------- /
       def update
-        todo = Todo.find(params[:id])
-        if todo.update_attributes(todo_params)
-          render json: {status: 'SUCCESS', message: 'todo updated', data: todo}, status: :ok
+        if @todo.update_attributes(todo_params)
+          render json: {status: 'SUCCESS', message: 'todo updated', data: @todo}, status: :ok
         else
-          render json: {status: 'ERROR', message: 'update todo failed', data: todo.errors }, status: :unprocessable_entity
+          render json: {status: 'ERROR', message: 'update todo failed', data: @todo.errors }, status: :unprocessable_entity
         end
       end
 
@@ -45,9 +46,8 @@ module Api
       # DELETE
       # ---------------------------------------------------------------------- /
       def destroy
-        todo = Todo.find(params[:id])
-        todo.destroy
-        render json: {status: 'SUCCESS', message: 'todo deleted', data: todo}, status: :ok
+        @todo.destroy
+        render json: {status: 'SUCCESS', message: 'todo deleted', data: @todo}, status: :ok
       end
 
       private
@@ -57,6 +57,10 @@ module Api
       # ---------------------------------------------------------------------- /
       def todo_params
         params.permit(:title, :desc, :done)
+      end
+
+      def select_todo
+        @todo = Todo.find(params[:id])
       end
 
     end
